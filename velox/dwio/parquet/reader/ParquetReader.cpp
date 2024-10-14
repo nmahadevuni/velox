f093814d57aed71c6187fbc17d787231cba86211
Mahadevuni Naveen Kumar <nmahadevuni@gmail.com>
2024-10-14 14:03:10 +0530

    Bloom filter read refactor

--- velox/dwio/parquet/reader/ParquetReader.cpp
+++ velox/dwio/parquet/reader/ParquetReader.cpp
@@ -982,6 +982,9 @@ class ParquetRowReader::Impl {
       }
     }
 
+    auto newInput = readerBase_->bufferedInput().clone();
+    newInput->load(dwio::common::LogType::STRIPE_FOOTER);
+
     ParquetData::FilterRowGroupsResult res;
     columnReader_->filterRowGroups(0, parquetStatsContext_, res);
     if (auto& metadataFilter = options_.metadataFilter()) {
@@ -1075,20 +1078,6 @@ class ParquetRowReader::Impl {
     return readerBase_->isRowGroupBuffered(rowGroupIndex);
   }
 
-  std::shared_ptr<BloomFilter> getBloomFilter(
-      const uint32_t rowGroupId,
-      const uint32_t column) {
-    VELOX_CHECK_LT(
-        column,
-        columnReader_->children().size(),
-        "Invalid column index at column ordinal : {}",
-        column);
-    return columnReader_->children()[column]
-        ->formatData()
-        .as<ParquetData>()
-        .getBloomFilter(rowGroupId);
-  }
-
  private:
   bool advanceToNextRowGroup() {
     if (nextRowGroupIdsIdx_ == rowGroupIds_.size()) {
@@ -1174,12 +1163,6 @@ std::optional<size_t> ParquetRowReader::estimatedRowSize() const {
 
 const thrift::FileMetaData& ParquetRowReader::fileMetaData() const {
   return impl_->fileMetaData();
-}
-
-std::shared_ptr<BloomFilter> ParquetRowReader::getBloomFilter(
-    const uint32_t rowGroupId,
-    const uint32_t column) {
-  return impl_->getBloomFilter(rowGroupId, column);
 }
 
 ParquetReader::ParquetReader(

713470968cf3d484403edc5985d03dfbf52d8858
Mahadevuni Naveen Kumar <nmahadevuni@gmail.com>
2025-10-08 12:41:42 +0530

    refactor(iceberg): Remove old iceberg references from hive files

--- velox/connectors/hive/SplitReader.cpp
+++ velox/connectors/hive/SplitReader.cpp
@@ -21,7 +21,6 @@
 #include "velox/connectors/hive/HiveConnectorSplit.h"
 #include "velox/connectors/hive/HiveConnectorUtil.h"
 #include "velox/connectors/hive/TableHandle.h"
-#include "velox/connectors/hive/iceberg/IcebergSplitReader.h"
 #include "velox/dwio/common/ReaderFactory.h"
 
 namespace facebook::velox::connector::hive {
@@ -99,24 +98,6 @@ std::unique_ptr<SplitReader> SplitReader::create(
     const std::shared_ptr<common::ScanSpec>& scanSpec,
     core::ExpressionEvaluator* expressionEvaluator,
     std::atomic<uint64_t>& totalRemainingFilterTime) {
-  //  Create the SplitReader based on hiveSplit->customSplitInfo["table_format"]
-  if (hiveSplit->customSplitInfo.count("table_format") > 0 &&
-      hiveSplit->customSplitInfo["table_format"] == "hive-iceberg") {
-    return std::make_unique<iceberg::IcebergSplitReader>(
-        hiveSplit,
-        hiveTableHandle,
-        partitionKeys,
-        connectorQueryCtx,
-        hiveConfig,
-        readerOutputType,
-        ioStats,
-        fsStats,
-        fileHandleFactory,
-        ioExecutor,
-        scanSpec,
-        expressionEvaluator,
-        totalRemainingFilterTime);
-  } else {
     return std::unique_ptr<SplitReader>(new SplitReader(
         hiveSplit,
         hiveTableHandle,
@@ -129,7 +110,6 @@ std::unique_ptr<SplitReader> SplitReader::create(
         fileHandleFactory,
         ioExecutor,
         scanSpec));
-  }
 }
 
 SplitReader::SplitReader(
